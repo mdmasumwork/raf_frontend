22477821f48aca16b81afbfd44ad070bb3f2f353
Md Masum <masum@talentedcube.com>
2023-10-12 20:54:51 -0400

    Form validation is added with custom validation.

--- src/app/features/pages/create-product/create-product.component.ts
+++ src/app/features/pages/create-product/create-product.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ControlContainer, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -13,20 +13,22 @@ export class CreateProductComponent {
   
   isPrimeValueChangeObservable: Subscription | null = null;
   showAdditionalPriceField: boolean = false;
+  formSubmitted: boolean = false;
 
   constructor(private formBuilder: FormBuilder) {
     this.formGroup = formBuilder.group({
-      name: [''],
-      detail: [''],
-      price: [0],
-      isPremium: [],
-      additionalCharge: [0]
+      name: ['', [this.emptyValidator]],
+      detail: ['', [this.emptyValidator]],
+      price: [0, [this.priceValidator]],
+      isPremium: ["NO"],
+      additionalCharge: [0, [this.additionalChargeValidator]]
     });
     this.categoryField = new FormControl();
   }
 
   ngOnInit(): void {
     this.isPrimeValueChangeObservable = this.formGroup.controls['isPremium'].valueChanges.subscribe(value => {
+      this.formGroup.get('additionalCharge')?.updateValueAndValidity();
       if(value === "YES") {
         this.showAdditionalPriceField = true;
       } else {
@@ -36,16 +38,33 @@ export class CreateProductComponent {
   }
   
   createProductHandler(): void {
-
-    const productData = this.formGroup.value;
-
-    console.log(productData);
-    console.log(this.categoryField.value);
+    this.formSubmitted = true;
+    if(this.formGroup.valid) {
+      console.log("Form is valid");
+    } else {
+      console.log("Form is not valid")
+    }
   }
 
   ngOnDestroy(){
     if (this.isPrimeValueChangeObservable) {
       
     }
+  }
+
+  emptyValidator(control: FormControl) {
+    if (control.value.trim().length === 0) {
+      return {'emptyValidator': true};
+    } else {
+      return null;
+    }
+  }
+
+  priceValidator(conntrol: FormControl) {
+    return conntrol.value < 0 ? {'priceValidator': true} : null;
+  }
+
+  additionalChargeValidator(control: FormControl) {
+    return (control.root.get('isPremium')?.value === "YES" && control?.value < 0) ? {'additionalChargeValidator': true} : null;
   }
 }
